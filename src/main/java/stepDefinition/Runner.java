adc0dd727ae823f331ce1e6b8a21aa92d166cf01
sumit45 <sumit619.hellboy@gmail.com>
2024-04-11 17:55:11 +0530

    DataTable Concept

--- src/main/java/stepDefinition/Runner.java
+++ src/main/java/stepDefinition/Runner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\91958\\eclipse-workspace\\com.cucumber\\src\\main\\resources\\feature\\Amazon.feature",
-				glue="stepDefinition")
+@CucumberOptions(features="C:\\Users\\91958\\eclipse-workspace\\com.cucumber\\src\\main\\resources\\feature\\DatatableExample.feature",
+				glue="stepDefinition",
+				tags="@Datatable")
 	
 public class Runner {
 
